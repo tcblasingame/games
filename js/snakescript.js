59111cdff0ad63eaef7095c44f361814dfa0a961
Timothy (Clay) Blasingame <timothy.blasingame@okstate.edu>
2023-11-09 21:17:40 -0600

    update

--- js/snakescript.js
+++ js/snakescript.js
@@ -1,7 +1,7 @@
 const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
 const highScoreElement = document.querySelector(".high-score");
-const controls = document.querySelectorAll(".controls i");
+const controls = document.querySelectorAll(".snakecontrols i");
 
 let gameOver = false;
 let foodX, foodY;
